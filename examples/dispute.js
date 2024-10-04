f5630315faae25452500154982cd584dfada8d89
Jake Merringer <jakemerringer@gmail.com>
2024-10-04 14:07:41 -0500

    add dispute example

---
+++ examples/dispute.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(
+  '/scripts',
+  express.static(__dirname + '/../node_modules/@justifi/webcomponents/dist/')
+);
+app.use('/styles', express.static(__dirname + '/../css/'));
+
+
+app.get('/', async (req, res) => {
+
+  res.send(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>JustiFi Checkout</title>
+        <script type="module" src="/scripts/webcomponents/webcomponents.esm.js"></script>
+        <link rel="stylesheet" href="/scripts/webcomponents/webcomponents.css">
+        <link rel="stylesheet" href="/styles/theme.css">
+        <link rel="stylesheet" href="/styles/example.css">
+      </head>
+      <body>
+        <div style="margin:0 auto;max-width:600px;">
+          <justifi-dispute-management></justifi-dispute-management>
+        </div>
+      </body>
+    </html>
+  `);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
